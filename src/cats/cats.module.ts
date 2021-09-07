71400148653ebedb7d382c8c6a111e719a2154e5
Guido Perdomo <guidop91@gmail.com>
2021-09-06 20:42:04 -0400

    route everything better

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 
 @Module({
   controllers: [CatsController],
   providers: [CatsService],
+  exports: [CatsService],
 })
 export class CatsModule {}
